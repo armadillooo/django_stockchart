4e666251944486142e8953a4d1f85ae4566dd5ce
armadillooo <57446967+armadillooo@users.noreply.github.com>
2019-12-09 19:04:30 +0900

    move my_venv

--- my_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\abcha\documents\myapplications\django_stock\my_venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
