f171256d1ec05e1e74982771fd95aeb1014e1625
armadillooo <57446967+armadillooo@users.noreply.github.com>
2019-12-09 19:01:43 +0900

    change .gitignore

---
+++ my_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abcha\documents\myapplications\django_stock\my_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
