e0b9494fc2ca3a626e79b9b6cbf62bcf42405581
armadillooo <57446967+armadillooo@users.noreply.github.com>
2019-12-25 03:20:07 +0900

    add / to accounts.urls my_page/

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('favorite/', views.favorite, name='favorite'),
-    path('my_page', views.my_page, name='my_page'),
+    path('my_page/', views.my_page, name='my_page'),
 ]
